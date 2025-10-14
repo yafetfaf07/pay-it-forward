4564cbf602ef9c409d9fcbdb2c3003d0534eea5a
Yafet Yosef <japhetkumalo23@gmail.com>
2025-10-14 17:15:44 +0300

    Add TypeScript types for request and response in the Express app

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
-app.use("/", (req, res) => {
+app.use("/", (req:Request, res:Response) => {
   res.status(200).json({ message: "Api worked" });
 });
 app.listen(5000, () => {
